56204e2be868a5744eb235d4ff90a9f787395ee7
ZiomaleQ <r.partyka30@gmail.com>
2023-10-21 14:28:50 +0200

    Let's go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tfrpc
+
+go 1.21.3
+
+require (
+	github.com/hugolgst/rich-go v0.0.0-20230917173849-4a4fb1d3c362 // direct
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
---
+++ mod.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/hugolgst/rich-go/client"
+)
+
+type Process struct {
+	Name        string `json:"Name"`
+	WindowTitle string `json:"mainWindowTitle"`
+	StartTime   string `json:"StartTime"`
+}
+
+func main() {
+	err := client.Login("622783718783844356")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		processes := getProcessList()
+
+		worthProcesses := make([]Process, 0)
+
+		for _, process := range processes {
+			if process.Name == "crosvm" {
+				worthProcesses = append(worthProcesses, process)
+			}
+		}
+
+		tsInt, err := strconv.ParseInt(worthProcesses[0].StartTime, 10, 64)
+
+		if err != nil {
+			panic(err)
+		}
+
+		ts := time.Unix(tsInt, 0)
+
+		client.SetActivity(client.Activity{
+			State:      "Gra z Google Play",
+			Details:    "Arknights",
+			LargeImage: "arknights",
+			LargeText:  "Arknights",
+			SmallImage: "arknights",
+			SmallText:  "Arknights",
+			Timestamps: &client.Timestamps{
+				Start: &ts,
+			},
+		})
+
+		time.Sleep(15 * time.Second)
+	}
+}
+
+func getProcessList() []Process {
+	size := exec.Command("powershell.exe", "-Command", "(Get-Process | Where-Object {$_.mainWindowTitle} ).Count")
+	cmd := exec.Command("powershell.exe", "-Command", "Get-Process | Where-Object {$_.mainWindowTitle} | Select Name, mainWindowTitle, StartTime | ConvertTo-Json -Compress")
+
+	rawOut, err := cmd.Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	rawSize, err := size.Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	processList := make([]Process, int(rawSize[0]))
+
+	json.Unmarshal([]byte(rawOut), &processList)
+
+	for idx, process := range processList {
+		processList[idx].StartTime = process.StartTime[strings.IndexRune(process.StartTime, '(')+1 : strings.IndexRune(process.StartTime, ')')]
+	}
+
+	return processList
+}
